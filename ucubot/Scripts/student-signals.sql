2f2aa6aaec77028cfbb7213921dbe7ef43e97e1b
mhirna <m.hirna@ucu.edu.ua>
2018-06-04 19:01:10 +0300

    Fix view

--- ucubot/Scripts/student-signals.sql
+++ ucubot/Scripts/student-signals.sql
@@ -3,7 +3,8 @@ USE ucubot;
 CREATE VIEW student_signals AS
     SELECT student.first_name, student.last_name,
         (CASE lesson_signal.SignalType WHEN -1 THEN "Simple" WHEN 0 THEN "Normal" WHEN 1 THEN "Hard" END) AS signal_type,
-            COUNT(lesson_signal.student_id) AS count
-    FROM student
-    JOIN lesson_signal ON lesson_signal.student_id = student.id
-    GROUP BY lesson_signal.SignalType, student.user_id;
+            COUNT(*) AS count
+    FROM lesson_signal
+    INNER JOIN student ON student.id = lesson_signal.student_id
+    GROUP BY signal_type, student_id;
+
