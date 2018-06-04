a3c545c22c36b749f543a0fda965752bf9e7d646
mhirna <m.hirna@ucu.edu.ua>
2018-06-04 19:02:31 +0300

    Update StudentSignalRepository.cs

--- ucubot/Databases/StudentSignalRepository.cs
+++ ucubot/Databases/StudentSignalRepository.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using Dapper;
 using MySql.Data.MySqlClient;
@@ -13,7 +13,8 @@ namespace ucubot.Databases
             using (var conn = new MySqlConnection(connectionString))
             {
                 conn.Open();
-                var command = "SELECT * FROM student_signals;";
+                var command = "SELECT any_value(student_signals.first_name) AS FirstName, any_value(student_signals.last_name) AS LastName, " +
+                              "any_value(student_signals.signal_type) AS SignalType, any_value(student_signals.count) AS Count FROM student_signals;";                
                 List<StudentSignal> signals = conn.Query<StudentSignal>(command).ToList();
                 return signals;
             }
